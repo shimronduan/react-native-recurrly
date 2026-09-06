5fde068fd4a02938a7f41410afc0a35328cc411a
shimronduan <shimron_duan@next.co.uk>
2026-09-06 10:54:52 +0100

    chore: update background color in Index component and add theme variables in global CSS

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="flex-1 items-center justify-center bg-background">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
