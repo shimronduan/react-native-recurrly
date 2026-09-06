8ba9aa721d84000107e89b033321e7de072a1a0f
shimronduan <shimron_duan@next.co.uk>
2026-09-06 18:31:07 +0100

    feat: add TabLayout and Index components for navigation setup

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,8 @@
+import { Tabs } from "expo-router";
+
+const TabLayout = () => (
+  <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs.Screen name="index" options={{ title: "Home" }} />
+  </Tabs>
+);
+export default TabLayout;
