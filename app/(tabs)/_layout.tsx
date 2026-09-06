37d558e08c036c5a67016771af676ed52b79ecdf
shimronduan <shimron_duan@next.co.uk>
2026-09-06 19:44:00 +0100

    feat: add constants for icons, theme, and tab data
    - Created a new file `data.ts` to define application tabs with associated icons.
    - Introduced `icons.ts` to manage icon imports and exports for better organization.
    - Added `theme.ts` to define color palette, spacing, and component dimensions for consistent styling.
    - Included `image.d.ts` to declare module types for image assets.
    - Defined global types in `type.d.ts` for application tabs, subscriptions, and related components.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,45 @@
+import { tabs } from "@/constants/data";
+import clsx from "clsx";
 import { Tabs } from "expo-router";
+import { Image, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-const TabLayout = () => (
-  <Tabs screenOptions={{ headerShown: false }}>
-    <Tabs.Screen name="index" options={{ title: "Home" }} />
-  </Tabs>
-);
+const TabLayout = () => {
+  const insets = useSafeAreaInsets();
+  const TabIcon = ({ focused, icon }: TabIconProps) => {
+    return (
+      <View className="tabs-icon">
+        <View className={clsx("tabs-pill", focused && "tabs-active")}>
+          <Image source={icon} className="tabs-glyph" />
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: "absolute",
+          Math.max(insets.bottom, 10),
+        },
+      }}
+    >
+      {tabs.map((tab) => (
+        <Tabs.Screen
+          key={tab.name}
+          name={tab.name}
+          options={{
+            title: tab.title,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon focused={focused} icon={tab.icon} />
+            ),
+          }}
+        />
+      ))}
+    </Tabs>
+  );
+};
 export default TabLayout;
